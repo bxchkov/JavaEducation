d52774c36f43f0bb23fbee3cfefd1f827f7a1a0a
PC <qtqt007@mailru>
2022-12-10 03:39:03 +0500

    timus

---
+++ src/timus/task1293/Main.java
@@ -0,0 +1,14 @@
+package timus.task1293;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int answer = (n*(a*b))*2;
+    }
+}
+
