9cc88ad460e27f8e655729a7f55e02743db48c9b
PC <qtqt007@mailru>
2022-12-10 03:00:30 +0500

    test)

--- src/lr1/Example9.java
+++ src/lr1/Example9.java
@@ -3,6 +3,7 @@ package lr1;
 import java.util.Scanner;
 public class Example9 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Input current month: ");
